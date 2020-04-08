af0f5b2d22037e734b0828a9ed0630934a0f134b
Himani Dalal <57623122+himani027@users.noreply.github.com>
2020-04-08 18:27:25 +0530

    Update Q3.py

--- Question 3/Answer/Q3.py
+++ Question 3/Answer/Q3.py
@@ -5,14 +5,12 @@ def is_triplet(a,b,c):
         return True
     else:
         return False
-def main():
+
     sum_total = 1000
-    
-    for c in range(sum_total):
-        for b in range(c):
-            for a in range(b):
-                if a + b + c == sum_total:
-                    if is_triplet(a,b,c):
-                        # print(a,b,c)
-                        print(a*b*c) 
-main()
+for c in range(sum_total):
+    for b in range(c):
+        for a in range(b):
+            if a + b + c == sum_total:
+                if is_triplet(a,b,c):
+                    # print(a,b,c)
+                    print(a*b*c) 
