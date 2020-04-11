acb28ace44f9fb28c86bb0059dfe579fb032237b
Himani Dalal <57623122+himani027@users.noreply.github.com>
2020-04-12 00:16:26 +0530

    Create Q8.py

---
+++ Question 8/Answer/Q8.py
@@ -0,0 +1,14 @@
+# Himani Dalal
+
+def sum_of_digits(num):
+	sum = 0
+	while(num > 0):
+  	    rem = num % 10
+        sum += rem
+        num //= 10
+    return sum
+
+num = 2 ** 1000  # 2 power 1000
+result = sum_of_digits(num)
+#print(num)
+print("Sum of the digits of 2**1000 is ",result)
